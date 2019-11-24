f89aa619e01361df9c8e3b042525caca031766b0
YuFeiran <yufeiran1999@gmail.com>
2019-11-24 23:06:17 +0800

    second push

---
+++ TinyCompiler/util.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef _UTIL_H_
+#define UTIL_H_
+#include"globals.h"
+/* Procedure printToken prints a token 
+* and tis lexeme to the listing file
+*/
+void printToken(TokenType, const char);
+
+/* Function newStmtNode creates a new statement
+ * node for syntax tree construction 
+ */
+TreeNode* newStmtNode(StmtKind);
+
+/* Function newExpNode creates a new expression
+ * node for syntax tree construction
+ */
+TreeNode* newExpNode(ExpKind);
+
+/* Funtion copyString allocates and makes a new 
+ * copy of an existing string 
+ */
+char* copyString(char*);
+
+/* procedure printTree prints a syntax tree to the
+ * listing file using indentation to indicate subtrees
+ */
+void printTree(TreeNode);
+#endif
